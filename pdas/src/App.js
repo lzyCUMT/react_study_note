8f46f59e053f42242135fa45afd4712ee5cdcc52
lzyCUMT <3555245738@qq.com>
2025-10-15 19:49:40 +0800

    feat(pdas): add initial project structure and components
    - Add .gitignore file to exclude build artifacts and dependencies
    - Create App.js with ProfileCard components
    - Create ProfileCard.js with basic profile card implementation
    - Set up index.js to render the React application
    - Include public assets and configuration files for the pdas project

---
+++ pdas/src/App.js
@@ -0,0 +1,14 @@
+import ProfileCard  from "./ProfileCard";
+
+function App() {
+    return (
+        <div>
+            <div>Personal Digital Assitants</div>
+            <ProfileCard />
+            <ProfileCard />
+            <ProfileCard />
+        </div>
+    )
+}
+
+export default App;
