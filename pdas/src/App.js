fdfa1bc715b164634539f39c83e2fe528311b684
lzyCUMT <3555245738@qq.com>
2025-10-15 20:12:31 +0800

    feat(pdpa): add title and handle props to ProfileCard component
    Pass title and handle properties to ProfileCard components in App.js
    and update ProfileCard to accept and display these props. This change
    enhances the ProfileCard component to show dynamic content instead
    of static text.

--- pdas/src/App.js
+++ pdas/src/App.js
@@ -1,14 +1,14 @@
-import ProfileCard  from "./ProfileCard";
+import ProfileCard from "./ProfileCard";
 
 function App() {
-    return (
-        <div>
-            <div>Personal Digital Assitants</div>
-            <ProfileCard />
-            <ProfileCard />
-            <ProfileCard />
-        </div>
-    )
+  return (
+    <div>
+      <div>Personal Digital Assitants</div>
+      <ProfileCard title="Alexa" handle="@alexa99" />
+      <ProfileCard title="Cortana" handle="@cortana32" />
+      <ProfileCard title="Siri" handle="@siri01" />
+    </div>
+  );
 }
 
 export default App;
