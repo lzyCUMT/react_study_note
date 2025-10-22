460eb7591ce6fbfbe5e93fa986a59e9cceb7bfee
lzyCUMT <3555245738@qq.com>
2025-10-22 19:57:02 +0800

    feat(comps): add classnames and react-icons dependencies
    - Install `classnames` and `react-icons` packages
    - Update Button component to use classnames for dynamic styling
    - Add conditional CSS classes based on props (primary, secondary, etc.)
    - Implement outline and rounded style variants
    - Add GoBell icon from react-icons to App.js
    - Style button icons with margin index.css

--- comps/src/App.js
+++ comps/src/App.js
@@ -1,9 +1,11 @@
+import { GoBell } from "react-icons/go";
 import Button from "./Button";
 function App() {
   return (
     <div>
       <div>
         <Button>Click here!</Button>
+        <GoBell />
       </div>
       <div>
         <Button>Click here!</Button>
